68e8c1bc0f3e63270c8169c61333a58e7c12109c
mandadi-madhu <contractor.madhu@gmail.com>
2019-11-02 21:41:13 +0000

    use json to true

--- darksky-request.js
+++ darksky-request.js
@@ -2,7 +2,7 @@ const request = require('request');
 
 const darkSkyUrl = "https://api.darksky.net/forecast/3e015c1a66447546e7f9e36ef3d25a22/37.8267,-122.4233";
 
-request({ url: darkSkyUrl}, (error, response) => {
-    const data = JSON.parse(response.body);
-    console.log(data);
+request({ url: darkSkyUrl, json: true}, (error, response) => {
+    const currTemp = `Current temperature here is ${response.body.currently.temperature}`;
+    console.log(currTemp);
 });
