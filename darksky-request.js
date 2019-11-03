1265d76a16e967639077d4102af441e661013583
mandadi-madhu <contractor.madhu@gmail.com>
2019-11-03 16:26:39 +0000

    callback chaining

--- darksky-request.js
+++ darksky-request.js
@@ -1,18 +1,17 @@
 const getGeoCodes = require('./utils/geoCode');
 const getForecast = require('./utils/forecast');
 
-getGeoCodes('London', (error, response) => {
+getGeoCodes('London', (error, geoData) => {
     if (error) {
-        console.log(error);
+        return console.log(error);
     } else {
-        console.log(response);
-    }
-});
-
-getForecast('51.50722', '-0.1275', (error, response) => {
-    if (error) {
-        console.log(error);
-    } else {
-        console.log(response);
+        getForecast(geoData.lattitude, geoData.langitude, (error, forecastData) => {
+            if (error) {
+                return console.log(error);
+            } else {
+                console.log(geoData.placeName);
+                console.log(forecastData);
+            }
+        });
     }
 });
