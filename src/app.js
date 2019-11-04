cc2d97504d03d47e97cc9fbf917796801098ee46
mandadi-madhu <contractor.madhu@gmail.com>
2019-11-04 22:08:38 +0000

    Use handle-bars for rendering dynamic HTML pages

--- src/app.js
+++ src/app.js
@@ -1,15 +1,30 @@
 const express = require('express');
-const path = require('path');
+// const path = require('path');
 
-const publicFolderPath = path.join(__dirname, '../public');
+// const publicFolderPath = path.join(__dirname, '../public');
 
 const app = express();
-app.use(express.static(publicFolderPath));
+// app.use(express.static(publicFolderPath));
 
-app.get('/data', (req, res) => {
-    res.send({
-        name: 'madhu',
-        type: 'fun'
+app.set('view engine', 'hbs');
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather App',
+        author: 'Me'
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About me',
+        author: 'Madhu'
+    });
+});
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        helpText: 'This is help text'
     });
 });
 
