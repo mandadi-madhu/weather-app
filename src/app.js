5cdc3276e6b95d84e785d9a4d02f3c9bf927096b
mandadi-madhu <contractor.madhu@gmail.com>
2019-11-04 15:05:48 +0000

    use of express static

--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const path = require('path');
+
+const publicFolderPath = path.join(__dirname, '../public');
 
 const app = express();
+app.use(express.static(publicFolderPath));
 
 app.get('/data', (req, res) => {
     res.send({
